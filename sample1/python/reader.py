31ea0ce8e15952b70fa9ba6c0c304280acd3dd36
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-06-08 09:31:57 +0900

    fir

--- sample1/python/reader.py
+++ sample1/python/reader.py
@@ -2,27 +2,28 @@
 
 
 """
-
 Protocol buffer reader in Python
 
 author: Atsushi Sakai
-
 """
 
 import addressbook_pb2
 
 
 def main():
+    """
+    main func
+    """
     print("start!!")
 
     address_book = addressbook_pb2.AddressBook()
 
     # Read the existing address book.
-    f = open("pbdata_py.dat", "rb")  # read data from python code
-    # f = open("../cpp/pbdata_cpp.dat", "rb")  # read data from cpp code
+    file_path = open("pbdata_py.dat", "rb")  # read data from python code
+    # file_path = open("../cpp/pbdata_cpp.dat", "rb")  # read data from cpp code
 
-    address_book.ParseFromString(f.read())
-    f.close()
+    address_book.ParseFromString(file_path.read())
+    file_path.close()
 
     print(address_book)
 
